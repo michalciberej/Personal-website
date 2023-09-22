a0fdd6a1f010cb85f228cff75a18841cfa95a89b
Michal Ciberej <michal.ciberej@gmail.com>
2023-09-22 16:58:24 +0200

    Refactored Socials component

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,47 +1,35 @@
-import { mdiGithub, mdiLinkedin, mdiTwitter } from '@mdi/js';
-import Icon from '@mdi/react';
-
-const Socials = () => {
+const Socials = ({ column, black }) => {
   return (
     <>
-      <ul className='flex gap-4 justify-center container m-auto'>
-        <li>
-          <a
-            className='socialsBtn border-2 border-black dark:border-white hover:dark:border-primary focus:dark:border-primary flex'
-            href='https://github.com/michalciberej'
-            target='_blank'
-            rel='noreferrer'>
-            <Icon
-              path={mdiGithub}
-              size={1}
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            className='socialsBtn border-2 border-black dark:border-white hover:dark:border-primary focus:dark:border-primary flex'
-            href='https://www.linkedin.com/in/michal-ciberej-a1b235247/'
-            target='_blank'
-            rel='noreferrer'>
-            <Icon
-              path={mdiLinkedin}
-              size={1}
-            />
-          </a>
-        </li>
-        <li>
-          <a
-            className='socialsBtn border-2 border-black dark:border-white hover:dark:border-primary focus:dark:border-primary flex'
-            href='https://twitter.com/michalciberej'
-            target='_blank'
-            rel='noreferrer'>
-            <Icon
-              path={mdiTwitter}
-              size={1}
-            />
-          </a>
-        </li>
-      </ul>
+      <div
+        className={`flex ${column ? 'flex-col' : 'flex-row'} gap-4 ${
+          black ? 'text-text' : 'text-textD'
+        }`}>
+        <a
+          href='https://github.com/michalciberej'
+          target='_blank'
+          rel='noreferrer'
+          aria-label='Link to my Github account'
+          className='outline-none transition-transform hover:scale-150 focus:scale-150 active:scale-150'>
+          <i className='devicon-github-original text-4xl'></i>
+        </a>
+        <a
+          href='https://www.linkedin.com/in/michal-ciberej-a1b235247/'
+          target='_blank'
+          rel='noreferrer'
+          aria-label='Link to my Linkedin account'
+          className='outline-none transition-transform hover:scale-150 focus:scale-150 active:scale-150'>
+          <i className='devicon-linkedin-plain text-4xl'></i>
+        </a>
+        <a
+          href='https://twitter.com/michalciberej'
+          target='_blank'
+          rel='noreferrer'
+          aria-label='Link to my Twitter account'
+          className='outline-none transition-transform hover:scale-150 focus:scale-150 active:scale-150'>
+          <i className='devicon-twitter-original text-4xl'></i>
+        </a>
+      </div>
     </>
   );
 };
