7a75677e24af1ce31a5d534f684eaa35f7bdb10f
Michal Ciberej <michal.ciberej@gmail.com>
2023-09-22 16:57:10 +0200

    Refactored Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,110 +1,142 @@
-import Icon from '@mdi/react';
-import { Link } from 'react-router-dom';
-import { mdiArrowRightThin, mdiChevronRight } from '@mdi/js';
 import SectionHeading from './SectionHeading';
+import StyledLink from './StyledLink';
 
 const Projects = () => {
-  const data = [
-    {
-      name: 'name',
-      thumbnail:
-        'https://monday.com/blog/wp-content/uploads/2022/02/Weekly-Schedule-thumbnail.jpg',
-      description: 'Messages, posting images, liking',
-      techs: [
-        './src/assets/react.svg',
-        './src/assets/nodejs.svg',
-        './src/assets/express.svg',
-        './src/assets/tailwind.svg',
-        './src/assets/mongodb.svg',
-        './src/assets/vite.svg',
-      ],
-      link: 'google.com',
-    },
-    {
-      name: 'name',
-      thumbnail:
-        'https://cdn.dribbble.com/users/4465419/screenshots/17231558/36_4x.jpg',
-      description: 'Messages, posting images, liking',
-      techs: [
-        './src/assets/react.svg',
-        './src/assets/nodejs.svg',
-        './src/assets/express.svg',
-        './src/assets/tailwind.svg',
-        './src/assets/mongodb.svg',
-        './src/assets/vite.svg',
-      ],
-      link: 'google.com',
-    },
-
-    {
-      name: 'name',
-      thumbnail:
-        'https://static.vecteezy.com/system/resources/previews/012/022/489/original/attractive-product-review-video-thumbnail-design-template-clickable-premium-vector.jpg',
-      description: 'Messages, posting images, liking',
-      techs: [
-        './src/assets/react.svg',
-        './src/assets/nodejs.svg',
-        './src/assets/express.svg',
-        './src/assets/tailwind.svg',
-        './src/assets/mongodb.svg',
-        './src/assets/vite.svg',
-      ],
-      link: 'google.com',
-    },
-  ];
-
   return (
-    <div className='container mx-auto flex flex-col mb-20'>
-      <SectionHeading text='Here are some of my most recent projects'>
-        RECENT WORK
-      </SectionHeading>
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-8 relative mb-10'></div>
-      <div className='flex justify-center md:justify-end mr-0 md:mr-10'>
-        <Link
-          to='/archive'
-          className='inline-flex group text-xl items-center mt-4 font-semibold leading-tight'>
-          All projects
-          <Icon
-            path={mdiArrowRightThin}
-            size={1}
-            className='group-hover:translate-x-1 transition-transform text-primary'
+    <section id='projects'>
+      <div className='flex flex-col items-start mx-[4vw] md:mx-[10vw] pb-40'>
+        <SectionHeading num='02'>Projects</SectionHeading>
+        <div className='flex flex-col pl-0 lg:pl-12 w-full py-8 gap-40'>
+          <Project
+            thumbnail='https://v2.seb-graf.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FcCzvwJjReaSMC0aSyjGO&w=1920&q=75'
+            alt='project'
+            header='My Awesome Project'
+            description={
+              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nisi quo obcaecati neque quidem, dignissimos nobis tempore, cupiditate nesciunt ipsum laborum doloribus itaque nihil possimus ad enim eos accusantium facere.'
+            }
+            left={true}
+            links={[
+              { title: 'Github', link: 'xyz' },
+              { title: 'Live Demo', link: 'abc' },
+            ]}
           />
-        </Link>
+          <Project
+            thumbnail='https://v2.seb-graf.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FcCzvwJjReaSMC0aSyjGO&w=1920&q=75'
+            alt='project'
+            header='My Awesome Project'
+            description={
+              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nisi quo obcaecati neque quidem, dignissimos nobis tempore, cupiditate nesciunt ipsum laborum doloribus itaque nihil possimus ad enim eos accusantium facere.'
+            }
+            left={false}
+            links={[
+              { title: 'Github', link: 'xyz' },
+              { title: 'Live Demo', link: 'abc' },
+            ]}
+          />
+          <Project
+            thumbnail='https://v2.seb-graf.com/_next/image?url=https%3A%2F%2Fmedia.graphassets.com%2FcCzvwJjReaSMC0aSyjGO&w=1920&q=75'
+            alt='project'
+            header='My Awesome Project'
+            description={
+              'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus nisi quo obcaecati neque quidem, dignissimos nobis tempore, cupiditate nesciunt ipsum laborum doloribus itaque nihil possimus ad enim eos accusantium facere.'
+            }
+            left={true}
+            links={[
+              { title: 'Github', link: 'xyz' },
+              { title: 'Live Demo', link: 'abc' },
+            ]}
+          />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-const Project = ({ thumbnail, description, children, link }) => {
+const Project = ({ thumbnail, alt, header, description, links, left }) => {
   return (
     <>
-      <figure className='group relative rounded-xl bg-secondary overflow-hidden h-full'>
-        <img
-          src={thumbnail}
-          alt=''
-          className='rounded-xl group-hover:scale-110 group-hover:opacity-0 overlay h-full'
-        />
-        <figcaption className='absolute flex flex-col justify-around items-center font-semibold tracking-tight top-0 left-0 right-0 bottom-0'>
-          <h1 className='text-2xl opacity-0 group-hover:opacity-100 overlay'>
-            {children}
-          </h1>
-          <p className='text-lg font-semibold tracking-tight opacity-0 group-hover:opacity-100 overlay'>
-            {description}
-          </p>
-          <a
-            href={link}
-            target='_blank'
-            rel='noreferrer'
-            className='opacity-0 group-hover:opacity-100 hover:bg-primary  overlay text-xl px-4 py-1 rounded-full border-2 border-primary  flex items-center group/link gap-1'>
-            Live Demo
-            <Icon
-              path={mdiChevronRight}
-              size={0.8}
-              className='group-hover/link:translate-x-1 transition-transform shadow-lg'
-            />
-          </a>
-        </figcaption>
-      </figure>
+      {left && (
+        <div className='flex w-full relative flex-col'>
+          <div className='relative md:mr-auto md:ml-0 xl:ml-20'>
+            <img
+              src={thumbnail}
+              alt={alt}
+              className='md:translate-x-4 w-[92vw] md:w-[100vw] max-w-[700px] lg:max-w-[600px] h-full brightness-75 relative z-30'></img>
+            <div className='md:translate-x-8 project-border border-2 bg-secondary/10 border-secondary w-[92vw] md:w-[100vw] max-w-[700px] lg:max-w-[600px] h-full absolute top-0 left-0'></div>
+          </div>
+          <div className='project-info flex flex-col justify-center md:absolute md:bottom-8 w-[100vw] max-w-[92vw] md:max-w-lg lg:max-w-xl lg:right-60 md:-mt-60 lg:-mt-0 lg:bottom-20 z-50'>
+            <h3 className='text-2xl text-text ml-4 bg-primary -mb-2 relative z-50 max-w-fit px-4 py-1'>
+              {header}
+            </h3>
+            <div className='bg-textD p-8 shadow-xl'>
+              <p className='text-lg md:text-xl text-text border-b-[1px] break-all pb-4 border-backgroundD/50 mb-4'>
+                {description}
+              </p>
+              <p className='flex  flex-wrap mb-8 gap-2 text-md md:text-lg text-sky-500 italic '>
+                <span className='px-2 py-1'>React</span>
+                <span className='px-2 py-1'>Typescript</span>
+                <span className='px-2 py-1'>Tailwindcss</span>
+                <span className='px-2 py-1'>NodeJS</span>
+              </p>
+              <div className='flex gap-8'>
+                {links.map((el, index) => (
+                  <StyledLink
+                    black={true}
+                    icon={true}
+                    border={true}
+                    key={index}
+                    link={el.link}
+                    currentSection={false}
+                    id={index}>
+                    {el.title}
+                  </StyledLink>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      {!left && (
+        <div className='flex w-full relative flex-col-reverse xl:flex-col md:mb-20'>
+          <div className='project-info flex flex-col justify-center md:absolute w-[100vw] max-w-[92vw] md:max-w-lg md:left-60 md:bottom-8 lg:mt-0 lg:max-w-xl lg:left-[15rem] xl:left-[18rem] lg:bottom-20 z-50'>
+            <h3 className='text-2xl md:text-2xl text-text mr-4 -mb-2 bg-primary max-w-fit px-4 py-1 place-self-end relative z-50'>
+              {header}
+            </h3>
+            <div className='bg-textD p-8 shadow-xl'>
+              <p className='text-lg md:text-xl text-text border-b-[1px] pb-4 border-backgroundD/50 mb-8'>
+                {description}
+              </p>
+              <p className='flex flex-wrap mb-4 gap-2 text-md md:text-lg text-sky-500 italic '>
+                <span className='px-2 py-1'>React</span>
+                <span className='px-2 py-1'>Typescript</span>
+                <span className='px-2 py-1'>Tailwindcss</span>
+                <span className='px-2 py-1'>NodeJS</span>
+              </p>
+              <div className='flex gap-8'>
+                {links.map((el, index) => (
+                  <StyledLink
+                    black={true}
+                    icon={true}
+                    border={true}
+                    key={index}
+                    link={el.link}
+                    currentSection={false}
+                    id={index}>
+                    {el.title}
+                  </StyledLink>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className='relative lg:ml-auto lg:mr-20'>
+            <img
+              src={thumbnail}
+              alt={alt}
+              className='md:translate-x-4 w-[92vw] lg:w-[100vw] max-w-[700px] lg:max-w-[600px] h-full brightness-75 relative z-30'></img>
+            <div className='project-border border-2 bg-secondary/10 border-secondary w-[92vw] lg:w-[100vw] max-w-[700px] lg:max-w-[600px] h-full absolute top-0 left-0'></div>
+          </div>
+        </div>
+      )}
     </>
   );
 };
