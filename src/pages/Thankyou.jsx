a93d95b58841273286bfc5b5f0fa7e1ea7cda7e8
Michal Ciberej <michal.ciberej@gmail.com>
2023-09-22 17:06:15 +0200

    Refactored Thankyou page

--- src/pages/Thankyou.jsx
+++ src/pages/Thankyou.jsx
@@ -1,29 +1,13 @@
-import Icon from '@mdi/react';
-import { mdiCharity } from '@mdi/js';
-import { Link } from 'react-router-dom';
-
 const Thankyou = () => {
   return (
     <>
-      <div className='flex flex-col justify-center container m-auto items-center text-center my-44'>
-        <Icon
-          path={mdiCharity}
-          size={4}
-          className='text-primary mb-8'
-        />
-        <div>
-          <h1 className='text-4xl mb-4 font-bold'>
+      <div className='w-full h-screen bg-backgroundD flex flex-col gap-8 justify-center items-center text-center'>
+        <h1 className='flex flex-col gap-2'>
+          <span className='text-3xl text-primary'>
             Thank you for reaching out
-          </h1>
-          <h3 className='text-2xl mb-16'>
-            Will answer you as soon as possible
-          </h3>
-        </div>
-        <Link
-          to={'/'}
-          className='button text-xl border-2 text-text dark:text-textD hover:text-textD border-text dark:border-textD dark:hover:bg-textD dark:hover:text-text hover:bg-text focus:text-textD  dark:focus:text-text focus:bg-text dark:focus:bg-textD shadow-md'>
-          Go home
-        </Link>
+          </span>
+          <span className='text-2xl'>Will get to you asap</span>
+        </h1>
       </div>
     </>
   );
